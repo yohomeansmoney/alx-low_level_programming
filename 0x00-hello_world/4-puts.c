2d61836700f03f343a251928b6c66b8eeecd61b0
daniel nyoho <danielnyoho@gmail.com>
2022-12-09 13:55:09 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,11 @@
 #!/bin/bash
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main-Entry
+ * Return: Always 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+     puts("\"programming is like building a multi lingual puzzle);
+     return (0);
 }
