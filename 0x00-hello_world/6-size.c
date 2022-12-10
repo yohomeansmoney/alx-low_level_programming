d1af8a318b1cfc4d71d63b40a2a42764cc4d292e
daniel nyoho <danielnyoho@gmail.com>
2022-12-10 13:20:54 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of an int: %lu byte(S)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("SIZE OF a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	
 	return (0);
 }
