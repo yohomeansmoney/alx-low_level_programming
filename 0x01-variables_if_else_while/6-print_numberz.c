f142f18a05329b2fba3d9fa5a53c99c23b52c457
daniel nyoho <danielnyoho@gmail.com>
2022-12-12 06:36:28 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,20 +2,19 @@
 /**
  * main - Entry point
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	int c;
 
 	c = 0;
 	while
-		(c < 10){
-	i		putchar(c + '0');
-			c++;
+		(c < 10) {
+			putchar(c + '0');
+				c++;
 		}
-i	putchar('\n');
+	putchar('\n');
 	return (0);
 }
