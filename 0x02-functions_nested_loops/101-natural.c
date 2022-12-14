c002ecdb9f9af7541f5ee90d81d6a4f333236030
daniel nyoho <danielnyoho@gmail.com>
2022-12-14 03:44:50 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,12 +11,13 @@ int main(void)
 	int i, sum = 0;
 
 	for (i = 0; i < 1024; i++)
-		(
-		 if ((i % 3) == 0 || (i % 5) == 0)
-		 sum += i;
-		 }
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 
-		 printf("%d\n", sum);
+	printf("%d\n", sum);
 
-		 return (0);
-		 }
+	return (0);
+}
+		
