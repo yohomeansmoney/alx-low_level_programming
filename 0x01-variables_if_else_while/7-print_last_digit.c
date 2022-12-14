b4083b79ac4a08fe74731743617f2b1eb2bc6fe8
daniel nyoho <danielnyoho@gmail.com>
2022-12-13 22:00:46 -0800

    7-print_last_digit.c

---
+++ 0x01-variables_if_else_while/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number.
+ *  @n: The number whose the last digit will be returned.
+ *  Return: The last digit of n.
+ */
+int print_last_digit(int n)
+{
+	int a, r = 0;
+
+	if (n == INT_MIN)
+	{
+		n = INT_MAX;
+		r = 1;
+	}
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+	a = a + r;
+	_putchar('0' + a);
+	return (a);
+}
