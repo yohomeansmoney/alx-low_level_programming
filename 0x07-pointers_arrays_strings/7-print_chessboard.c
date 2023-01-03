ef766c67faa7d0513c36adf2a49a14ec1d3eef3b
daniel nyoho <danielnyoho@gmail.com>
2023-01-03 15:05:07 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: an 8 by 8 matrix
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
