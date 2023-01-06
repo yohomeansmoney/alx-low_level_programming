cba67ca9318d24ea83c3e23f667ce321067689f9
daniel nyoho <danielnyoho@gmail.com>
2023-01-05 16:38:25 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
-
 /**
  * strlen_no_wilds - Returns the length of a string,
  * ignoring wildcard characters.
- *  @str: The string to be measured.
- *
- *  Return: The length.
+ * @str: The string to be measured.
+ * Return: The length.
  */
+
 int strlen_no_wilds(char *str)
 {
 	int len = 0, index = 0;
@@ -24,7 +19,6 @@ int strlen_no_wilds(char *str)
 		index++;
 		len += strlen_no_wilds(str + index);
 	}
-
 	return (len);
 }
 
@@ -33,6 +27,7 @@ int strlen_no_wilds(char *str)
  * until it points to a non-wildcard character.
  * @wildstr: The string to be iterated through.
  */
+
 void iterate_wild(char **wildstr)
 {
 	if (**wildstr == '*')
@@ -47,11 +42,11 @@ void iterate_wild(char **wildstr)
  * another string potentially containing wildcards.
  * @str: The string to be matched.
  * @postfix: The postfix.
- *
- *Return: If str and postfix are identical - a pointer to the null byte
- located at the end of postfix.
-  Otherwise - a pointer to the first unmatched character in postfix.
+ * Return: If str and postfix are identical - a pointer to the null byte
+ * located at the end of postfix.
+ * Otherwise - a pointer to the first unmatched character in postfix.
  */
+
 char *postfix_match(char *str, char *postfix)
 {
 	int str_len = strlen_no_wilds(str) - 1;
@@ -65,7 +60,6 @@ char *postfix_match(char *str, char *postfix)
 		postfix++;
 		return (postfix_match(str, postfix));
 	}
-
 	return (postfix);
 }
 
@@ -73,10 +67,10 @@ char *postfix_match(char *str, char *postfix)
  * wildcmp - Compares two strings, considering wildcard characters.
  * @s1: The first string to be compared.
  * @s2: The second string to be compared - may contain wildcards.
- *
  * Return: If the strings can be considered identical - 1.
  * Otherwise - 0.
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*')
@@ -84,13 +78,9 @@ int wildcmp(char *s1, char *s2)
 		iterate_wild(&s2);
 		s2 = postfix_match(s1, s2);
 	}
-
 	if (*s2 == '\0')
 		return (1);
-
 	if (*s1 != *s2)
 		return (0);
-
 	return (wildcmp(++s1, ++s2));
 }
- 
